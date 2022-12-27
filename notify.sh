c0f9d131eaa840d075061e10d69b19bee4e1aa59
sadraliev <a.sadraliev@gmail.com>
2022-12-28 03:44:37 +0600

    remove wrong url

--- notify.sh
+++ notify.sh
@@ -2,13 +2,12 @@ slack_channel="#tb_solution_bots"
 slack_username="Frontend Dev Environment Bot"
 success=":rocket:"
 failure=":pensive:"
-slack_webhook_url="https://hooks.slack.com/services/"
 success_message="Tests passed successfully"
 failed_message="the tests failed"
 
 
 function run {
-    url="${slack_webhook_url}$1"
+    url="$1"
     message=$2
     emoji=$3
     curl -X POST --data-urlencode \
